caf08576c3749a2f8f286aedc25a97f7f81d11ae
aaronlelevier <aaron.lelevier@gmail.com>
2022-06-11 10:00:33 -0700

    ch4_ex1 geometry.erl

--- code/geometry.erl
+++ code/geometry.erl
@@ -7,7 +7,15 @@
 %%  Visit http://www.pragmaticprogrammer.com/titles/jaerlang2 for more book information.
 %%---
 -module(geometry).  
--export([area/1]). 
+-export([
+    area/1,
+    perimeter/1
+]).
 
 area({rectangle, Width, Height}) -> Width * Height;
-area({square, Side})             -> Side * Side.
+area({square, Side})             -> Side * Side;
+area({circle, Radius}) -> math:pi() * math:pow(Radius, 2).
+
+perimeter({rectangle, Width, Height}) -> Width*2 + Height*2;
+perimeter({square, Side}) -> Side*4;
+perimeter({circle, Radius}) -> 2 * math:pi() * Radius.
