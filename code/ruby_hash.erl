dd6fd0c90083c27c4cb80cb6be49c7648de888a1
aaronlelevier <aaron.lelevier@gmail.com>
2022-06-15 07:43:13 -0700

    ch5 ex3 - impl 1 func 'any' only

---
+++ code/ruby_hash.erl
@@ -0,0 +1,19 @@
+%% ---
+%%  ch5 ex3
+%%
+%%  Advanced: Look up the manual pages for the Ruby hash class.
+%%  Make a module of the methods in the Ruby class that you think
+%%  would be appropriate to Erlang.
+%%
+%%  See: https://ruby-doc.org/core-3.1.2/Hash.html
+%%---
+-module(ruby_hash).
+-export([
+    any/2
+]).
+
+any({Key, Value}, Map) ->
+    lists:member({Key, Value}, maps:to_list(Map));
+any(P, Map) when is_function(P) ->
+    maps:size(maps:filter(P, Map)) > 0;
+any(_Other, _Map) -> false.
