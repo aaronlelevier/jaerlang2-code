c16d945785af3be59783354a7013bfd4aff7c9da
aaronlelevier <aaron.lelevier@gmail.com>
2022-06-16 07:47:57 -0700

    ch6 ex2 - try_test.erl polite message and some actions on macros

--- code/try_test.erl
+++ code/try_test.erl
@@ -9,11 +9,26 @@
 -module(try_test).
 -compile(export_all).
 
+
+% ifdef macro pattern:
+% - https://www.erlang.org/doc/reference_manual/macros.html
+% - ch8#Macros
+
+% macro options:
+% https://www.erlang.org/doc/man/compile.html#file-2
+-define(mymacro, false).
+get_debug_info() -> ?mymacro.
+
 generate_exception(1) -> a;
 generate_exception(2) -> throw(a);
 generate_exception(3) -> exit(a);
 generate_exception(4) -> {'EXIT', a};
-generate_exception(5) -> error(a).
+generate_exception(5) ->
+    Arg = a,
+    case ?mymacro of
+        true -> error(Arg);
+        false -> {error, Arg}
+    end.
 
 demo1() ->
      [catcher(I) || I <- [1,2,3,4,5]].
@@ -30,11 +45,13 @@ catcher(N) ->
 demo2() ->
     [{I, (catch generate_exception(I))} || I <- [1,2,3,4,5]].
 
+% How to get stack trace:
+% https://stackoverflow.com/a/1336201/1913888
 demo3() ->
-    try generate_exception(5) 
+    try generate_exception(5)
     catch
-       error:X ->
-	    {X, erlang:get_stacktrace()}
+       error:X:StackTrace ->
+	    {X, StackTrace}
     end.
 	
 lookup(N) ->
